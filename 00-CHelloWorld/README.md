c651c4290ed4def37b8ce49410a6671ffcfc4b50
sebpb <sebastianperezbertoli@gmail.com>
2024-04-05 23:29:47 -0300

    Clean up and update hello.txt to output.txt

--- 00-CHelloWorld/README.md
+++ 00-CHelloWorld/README.md
@@ -1,4 +1,1 @@
-# Sintaxis y Semántica de los Lenguajes
-Comisión K2051, año 2024
-## Equipo
-A completar
+
--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,6 +1,12 @@
+/* hello.c
+ * Impresion en consola de "Hello World"
+ * Sebastián Pérez
+ * 5/4/24
+ */
+
 #include <stdio.h>
 
 int main() {
-    printf("Hola mundo");
+    printf("Hello World");
     return 0;
 }
